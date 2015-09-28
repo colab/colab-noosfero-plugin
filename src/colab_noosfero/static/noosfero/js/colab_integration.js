dbe513fa8549577399a2856ef57b90e4c4037507
Sergio Oliveira <sergio@tracy.com.br>
2015-09-28 12:49:53 -0300

    Check if variable is defined before calling JS

--- src/colab_noosfero/static/noosfero/js/colab_integration.js
+++ src/colab_noosfero/static/noosfero/js/colab_integration.js
@@ -11,33 +11,32 @@
 
 $(transform_tags);
 
-function  transform_tags()
-{
-       discussion_tag();
-       feed_gitlab_tag();
+function transform_tags() {
+  if (mailman_list) {
+    discussion_tag();
+    feed_gitlab_tag();
+  }
 }
 
-function feed_gitlab_tag()
-{
-       var $tag = $('#repository-feed-tab');
-       $tag.text("Esta comunidade não está associada a"+
-                 " nenhum repositório no momento, para mais"+
-                 " detalhes contate o administrador");
+function feed_gitlab_tag() {
+  var $tag = $('#repository-feed-tab');
+  $tag.text("Esta comunidade não está associada a" +
+       " nenhum repositório no momento, para mais" +
+       " detalhes contate o administrador");
 
-       $.getJSON(repository, {limit:activities_limit, offset:0},function(msg, e){
-             $tag.html(msg.html);
-             $tag.append("<div class=\"see-more-repository\">"+
-					   "<a href="+repository+">"+
-					   "veja toda a atividade no repositório"+
-					   "</a></div>");
-       });
+  $.getJSON(repository, {limit: activities_limit, offset: 0}, function(msg, e) {
+        $tag.html(msg.html);
+        $tag.append("<div class=\"see-more-repository\">" +
+          "<a href=" + repository + ">" +
+          "veja toda a atividade no repositório" +
+          "</a></div>");
+  });
 }
 
-function discussion_tag()
-{
-       var $tag = $('#discussions-tab');
-       var request_path = '/spb/get_list/'+
-                          '?list_name='+mailman_list+
-                          '&MAX='+list_limit;
-       $tag.load(request_path);
+function discussion_tag() {
+  var $tag = $('#discussions-tab');
+  var request_path = '/spb/get_list/' +
+                     '?list_name=' + mailman_list +
+                     '&MAX=' + list_limit;
+  $tag.load(request_path);
 }
