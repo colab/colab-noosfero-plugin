cb7ab51a146e3debdc6a02a06bf57c239ec6d9c9
Lucas Kanashiro <kanashiro.duarte@gmail.com>
2015-09-29 16:05:49 -0300

    Registered community_creation signal

--- src/colab_noosfero/apps.py
+++ src/colab_noosfero/apps.py
@@ -1,8 +1,15 @@
 
 from colab.plugins.utils.apps import ColabPluginAppConfig
+from colab.signals.signals import register_signal
 
 
 class NoosferoPluginAppConfig(ColabPluginAppConfig):
     name = 'colab_noosfero'
     verbose_name = 'Noosfero Plugin'
     namespace = 'noosfero'
+
+    registered_signals = ['community_creation']
+    short_name = 'noosfero'
+
+    def register_signal(self):
+        register_signal(self.short_name, self.registered_signals)
