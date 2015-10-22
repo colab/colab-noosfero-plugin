23a514252ea076b0bc73d9b56d5edba18f253406
Macartur Sousa <macartur.sc@gmail.com>
2015-10-22 17:06:43 -0200

    Altering search_preview template
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>
    Signed-off-by: Lucas Moura <lucas.moura28@gmail.com>
    Signed-off-by: Lucas Kanashiro <kanashiro.duarte@gmail.com>

--- src/colab_noosfero/templatetags/upstream.py
+++ src/colab_noosfero/templatetags/upstream.py
@@ -3,10 +3,16 @@ from django.conf import settings
 
 register = template.Library()
 
-@register.filter
-def get_upstream(value):
+@register.assignment_tag
+def get_upstream():
     return settings.COLAB_APPS['colab_noosfero']['upstream'][:-1]
 
-@register.filter
-def get_default_image_url(value):
-    return get_upstream(value)+"/images/icons-app/community-thumb.png"
+@register.assignment_tag
+def get_default_image_url():
+    return get_upstream()+"/images/icons-app/community-thumb.png"
+
+@register.assignment_tag
+def get_image_link_url(link):
+    if not link:
+        return get_default_image_url
+    return get_upstream()+link
