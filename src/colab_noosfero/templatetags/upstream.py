75a12ff67c2d46f8ed810aa6a9b3080ba7dae3c0
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-11-05 13:47:47 -0200

    Fixed dashboard search preview
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/colab_noosfero/templatetags/upstream.py
+++ src/colab_noosfero/templatetags/upstream.py
@@ -1,5 +1,7 @@
 from django import template
 from django.conf import settings
+from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
@@ -17,3 +19,12 @@ def get_image_link_url(link, size):
     if not link:
         return get_default_image_url(size)
     return get_upstream() + link
+
+
+@register.simple_tag()
+def get_author(username):
+    if username == '':
+        return ''
+    else:
+        profile = reverse('user_profile', kwargs={'username': username})
+        return _("By") + ' <a href="%s">%s</a>' % (profile, username)
