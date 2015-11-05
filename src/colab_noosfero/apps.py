db683fb02e2af45723e1be3e3d167e3cf9e178af
Lucas Moura <lucas.moura128@gmail.com>
2015-11-05 17:09:36 -0200

    Added namespace on plugin apps.py
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>

--- src/colab_noosfero/apps.py
+++ src/colab_noosfero/apps.py
@@ -5,3 +5,4 @@ from colab.plugins.utils.apps import ColabPluginAppConfig
 class NoosferoPluginAppConfig(ColabPluginAppConfig):
     name = 'colab_noosfero'
     verbose_name = 'Noosfero Plugin'
+    namespace = 'noosfero'
