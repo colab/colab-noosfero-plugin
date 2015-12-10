b30558e06ec831a774fd453dd2e35617e77dfd50
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-12-10 10:18:00 -0200

    Fixed noosfero community image link
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Lucas Kanashiro <kanashiro.duarte@gmail.com>
    Signed-off-by: Sérgio Oliveira <seocam@seocam.com>

--- src/colab_noosfero/templatetags/upstream.py
+++ src/colab_noosfero/templatetags/upstream.py
@@ -1,24 +1,24 @@
 from django import template
-from django.conf import settings
-from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
 
 register = template.Library()
 
 
-def get_upstream():
-    return settings.COLAB_APPS['colab_noosfero']['upstream'][:-1]
+def get_path(link):
+    return reverse('noosfero:noosfero', kwargs={'path': link.lstrip('/')})
 
 
 def get_default_image_url(size):
-    return get_upstream() + "/images/icons-app/community-{}.png".format(size)
+    return "/images/icons-app/community-{}.png".format(size)
 
 
-@register.simple_tag()
-def get_image_link_url(link, size):
+@register.simple_tag(takes_context=True)
+def get_image_link_url(context, link, size):
+    request = context['request']
     if not link:
-        return get_default_image_url(size)
-    return get_upstream() + link
+        link = get_default_image_url(size)
+    return request.build_absolute_uri(get_path(link))
 
 
 @register.simple_tag()
