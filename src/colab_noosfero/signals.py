2dccacc64741921cfc69b0fccc7c7404b43163f0
Gust <darksshades@hotmail.com>
2016-01-07 16:21:14 -0200

    Change signal to software community on create
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/colab_noosfero/signals.py
+++ src/colab_noosfero/signals.py
@@ -1,15 +1,15 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from colab_noosfero.models import NoosferoCommunity
+from colab_noosfero.models import NoosferoSoftwareCommunity
 from django.core.exceptions import ObjectDoesNotExist
 from colab.signals.signals import send
 
 
-@receiver(pre_save, sender=NoosferoCommunity)
+@receiver(pre_save, sender=NoosferoSoftwareCommunity)
 def verify_community_creation(sender, **kwargs):
     community = kwargs.get('instance')
     try:
-        NoosferoCommunity.objects.get(pk=community.id)
+        NoosferoSoftwareCommunity.objects.get(pk=community.id)
     except ObjectDoesNotExist:
         send('community_creation', 'noosfero', community=sender)
     else:
