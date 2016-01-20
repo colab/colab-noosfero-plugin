7d99943d73ee8315dd92b8a2ff5d1b38b61bfdef
Luiz Oliveira <ziuloliveira@gmail.com>
2016-01-20 16:25:13 -0200

    Inserting `_noosfero_session` on cookie when user log in
    Signed-off-by: Simião Carvalho <simiaosimis@gmail.com>
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/colab_noosfero/tasks.py
+++ src/colab_noosfero/tasks.py
@@ -1,3 +1,19 @@
+from urlparse import urljoin
+import requests
+import json
+
 
 def authenticate_user(sender, user, request, **kwargs):
-    pass
+    upstream = request.build_absolute_uri().split('/account/login')[0]
+    url = urljoin(upstream, 'social/')
+    remote_user_data = {}
+
+    remote_user_data['email'] = request.user.email
+    remote_user_data['name'] = request.user.get_full_name()
+
+    headers = {'HTTP_REMOTE_USER_DATA': json.dumps(
+        remote_user_data, sort_keys=True)}
+
+    request_noosfero = requests.get(url, headers=headers)
+    request.COOKIES.set('_noosfero_session',
+                        request_noosfero.cookies.get('_noosfero_session'))
