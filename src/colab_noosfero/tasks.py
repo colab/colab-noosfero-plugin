dc5c0e682ee2dddf5e9b64ef813e70f998451f05
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-01-25 16:49:11 -0200

    Added exception when noosfero is off
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/colab_noosfero/tasks.py
+++ src/colab_noosfero/tasks.py
@@ -1,13 +1,18 @@
 from colab_noosfero.views import NoosferoProxyView
+from urllib3.exceptions import MaxRetryError
 
 
 def authenticate_user(sender, user, request, **kwargs):
     proxy_view = NoosferoProxyView()
-    noosfero_request = proxy_view.dispatch(request, '/')
+    try:
+        noosfero_response = proxy_view.dispatch(request, '/')
+    except MaxRetryError:
+        # Couldn't connect to noosfero
+        return
 
-    if noosfero_request.status_code == 200:
+    if noosfero_response.status_code == 200:
         request.COOKIES.set('_noosfero_session',
-                            noosfero_request.cookies.get('_noosfero_session'))
+                            noosfero_response.cookies.get('_noosfero_session'))
 
 
 def logout_user(sender, user, request, **kwargs):
