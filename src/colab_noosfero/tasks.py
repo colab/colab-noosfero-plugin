e4a17c5c55d7a34fd4083afa5747126fbabaf670
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-01-25 13:11:24 -0200

    Refactored authenticate user
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>

--- src/colab_noosfero/tasks.py
+++ src/colab_noosfero/tasks.py
@@ -1,25 +1,10 @@
-from urlparse import urljoin
-from django.conf import settings
-import requests
-import json
+from colab_noosfero.views import NoosferoProxyView
 
 
 def authenticate_user(sender, user, request, **kwargs):
-    noosfero_settings = settings.COLAB_APPS.get('colab_noosfero')
-    prefix = noosfero_settings.get('urls').get('prefix')
-    prefix = prefix.replace('^', '')
+    proxy_view = NoosferoProxyView()
+    noosfero_request = proxy_view.dispatch(request, '/')
 
-    upstream = request.build_absolute_uri('/')
-    url = urljoin(upstream, prefix)
-
-    remote_user_data = {}
-    remote_user_data['email'] = request.user.email
-    remote_user_data['name'] = request.user.get_full_name()
-
-    headers = {'HTTP_REMOTE_USER_DATA': json.dumps(
-        remote_user_data, sort_keys=True)}
-
-    noosfero_request = requests.get(url, headers=headers)
     if noosfero_request.status_code == 200:
         request.COOKIES.set('_noosfero_session',
                             noosfero_request.cookies.get('_noosfero_session'))
