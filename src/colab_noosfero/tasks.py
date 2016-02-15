3c119ddf4bcc41178faf441cf9c414a28b64eb2c
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-02-15 16:29:46 -0200

    Logging exception
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Simião Carvalho <simiaosimis@gmail.com>

--- src/colab_noosfero/tasks.py
+++ src/colab_noosfero/tasks.py
@@ -1,5 +1,7 @@
 from colab_noosfero.views import NoosferoProxyView
 from urllib3.exceptions import MaxRetryError
+import logging
+logger = logging.getLogger(__name__)
 
 
 def authenticate_user(sender, user, request, **kwargs):
@@ -7,7 +9,7 @@ def authenticate_user(sender, user, request, **kwargs):
     try:
         noosfero_response = proxy_view.dispatch(request, '/')
     except MaxRetryError:
-        # Couldn't connect to noosfero
+        logger.info("Couldn't connect to noosfero")
         return
 
     if noosfero_response.status_code == 200:
