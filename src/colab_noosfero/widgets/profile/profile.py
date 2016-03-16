1255028a7d7b055fd92579c5b707676a083021c8
Macartur Sousa <macartur.sc@gmail.com>
2016-03-16 17:35:34 -0300

    Refactored to use widget profile
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- src/colab_noosfero/widgets/profile/profile.py
+++ src/colab_noosfero/widgets/profile/profile.py
@@ -1,60 +1,25 @@
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 from colab_noosfero.views import NoosferoProxyView, NoosferoProfileProxyView
-from colab.widgets.widget_manager import Widget
+from colab.widgets.widget_profile import ProfileWidget
 import re
 from django.utils.safestring import mark_safe
 
-class NoosferoProfileWidget(NoosferoProxyView, Widget):
+class NoosferoProfileWidget(NoosferoProxyView, ProfileWidget):
     identifier = 'noosfero_profile'
     name = _('Social')
-    colab_form = None
+    app_name = 'colab_noosfero'
 
-    def default_url(self):
-        return '/social/myprofile/macartur/profile_editor/edit'
-
-    def fix_url(self, url):
-        return re.sub('^.*/social/', '', url)
-
-    def is_colab_form(self, request):
-        if self.colab_form is None:
-            self.colab_form = request.POST.get('colab_form', False)
-        return self.colab_form
-
-    def must_respond(self, request):
-        return not self.is_colab_form(request) and '/social' in request.GET.get('path','')
+    def default_url(self, request):
+        user = request.user.username
+        return '{}/myprofile/{}/profile_editor/edit'.format(self.prefix, user)
 
     def dispatch(self, request, url):
         noosfero_proxy_view = NoosferoProfileProxyView()
         response = noosfero_proxy_view.dispatch(request, url)
 
         if response.status_code == 302:
-            url = self.fix_url(self.default_url())
+            url = self.fix_url(self.default_url(request))
             request.method = 'GET'
             response = noosfero_proxy_view.dispatch(request, url)
 
         return response
-
-    def change_request_method(self, request):
-        if not self.must_respond(request):
-            request.method = "GET"
-        else:
-            request.method = "POST"
-
-    def requested_url(self, request):
-        url = request.GET.get('path', '')
-
-        if not url or not self.must_respond(request):
-            url = self.default_url()
-
-        return self.fix_url(url)
-
-    def generate_content(self, **kwargs):
-        request = kwargs.get('context', {}).get('request', None)
-        self.change_request_method(request)
-        response = self.dispatch(request, self.requested_url(request))
-
-        if hasattr(response, 'content'):
-            self.content = response.content
-        else:
-            self.content = "".join(response.streaming_content)
