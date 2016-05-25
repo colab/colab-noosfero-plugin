b785d6f46fce981b3eb56861a4beaf3d5ad353fe
Macartur Sousa <macartur.sc@gmail.com>
2016-05-25 16:34:02 -0300

    Fixed login integration with noosfero
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- src/colab_noosfero/tasks.py
+++ src/colab_noosfero/tasks.py
@@ -1,19 +1,27 @@
-from colab_noosfero.views import NoosferoProxyView
-from urllib3.exceptions import MaxRetryError
+from colab.settings import COLAB_APPS
+import requests
 import logging
 logger = logging.getLogger(__name__)
 
 
+def get_upstream():
+    return COLAB_APPS.get('colab_noosfero', '').get('upstream', '')
+
+
+# TODO: need to use noosfero_proxy_view to make authentication,
+# requests was used because Noosfero only send a _noosfero_session
+# in the first request
 def authenticate_user(sender, user, request, **kwargs):
-    proxy_view = NoosferoProxyView()
+
     try:
-        noosfero_response = proxy_view.dispatch(request, '/profile')
-    except MaxRetryError:
+        headers = {'REMOTE_USER': user.username}
+        noosfero_response = requests.get(get_upstream(), headers=headers)
+    except:
         logger.info("Couldn't connect to noosfero")
         return
 
     if noosfero_response.status_code == 200:
-        session = noosfero_response.cookies.get('_noosfero_session').value
+        session = noosfero_response.cookies.get('_noosfero_session')
         request.COOKIES.set('_noosfero_session', session)
 
 
