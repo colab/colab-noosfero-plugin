7056db9702df0e3800a8f8d984625ed802d99a67
Macartur Sousa <macartur.sc@gmail.com>
2016-06-01 12:32:06 -0300

    Bump version 0.2.15 => 0.2.16
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = [
 
 setup(
     name="colab-noosfero",
-    version='0.2.15',
+    version='0.2.16',
     author='Sergio Oliveira',
     author_email='sergio@tracy.com.br',
     url='https://github.com/colab/colab-noosfero-plugin',
