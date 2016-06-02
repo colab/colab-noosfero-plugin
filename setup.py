4497be2d319a777f69345938e31a7d4c3e827081
Gustavo Jaruga Cruz <darksshades@hotmail.com>
2016-06-02 11:34:15 -0300

    Bump version 0.2.16 => 0.2.17

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = [
 
 setup(
     name="colab-noosfero",
-    version='0.2.16',
+    version='0.2.17',
     author='Sergio Oliveira',
     author_email='sergio@tracy.com.br',
     url='https://github.com/colab/colab-noosfero-plugin',
